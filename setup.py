8a1a387b5bfbeed53d446ce599d336d129d72597
rclawlor <73249568+rclawlor@users.noreply.github.com>
2022-11-08 17:28:24 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='event_touch',
+    packages=find_packages(include=['event_touch']),
+    version='0.1.0',
+    description='Event camera algorithms for optical touch sensing',
+    author='Ronan Lawlor',
+    license='MIT',
+)
